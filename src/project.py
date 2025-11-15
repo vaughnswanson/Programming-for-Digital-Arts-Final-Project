de0a31c5179884772becd609f609c15ce176b49d
Vaughn Swanson <vaughnswanson@gmail.com>
2025-11-15 17:50:05 -0600

    added self update

--- src/project.py
+++ src/project.py
@@ -5,6 +5,10 @@ class Enemy():
 
     def __init__(self, pos(0,0), health, speed):
         self.pos = pos
-        self.health = health
-        self.speed = speed
+        self.health = health #add random health from 1-5
+        self.speed = speed # assign random speed from 1-3
         self.alive = True
+
+    def update(self):
+        if self.health <= 0:
+            self.alive = False
