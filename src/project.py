5156af1e7a3813295b468867f00dbcdb6dace27f
Vaughn Swanson <vaughnswanson@gmail.com>
2025-11-16 16:16:21 -0600

    was forgetting to draw the freaks.. fixed it

--- src/project.py
+++ src/project.py
@@ -35,20 +35,22 @@ class Freak():
         #load sprite
         self.sprite = pygame.image.load("assets/images/freak1.png").convert_alpha()
         #scale sprite
-        self.sprite = pygame.transform.scale(self.sprite, (1, 1))
+        self.sprite = pygame.transform.scale(self.sprite, (50, 50))
         #give sprite hitbox
         self.rect = self.sprite.get_rect(topleft=pos)
 
 
-    def update(self):
+    def update(self, dt):
         if self.health <= 0:
             self.alive = False
+
             # if freak moves off scereen delete it
         if (self.pos[0] < 0 or self.pos[0] > 800 or
             self.pos[1] < 0 or self.pos[1] > 600):
             self.alive = False
 
     def draw(self, screen):
+        pygame.draw.rect(screen, (255,0,0), self.rect)
         #draw freak sprite at its position
         screen.blit(self.sprite, self.rect.topleft)       
 
@@ -59,10 +61,10 @@ class Bullet():
         self.speed = speed
         self.alive = True
 
-    def update(self):
+    def update(self, dt):
         # Update bullet position based on direction and speed
-        self.pos = (self.pos[0] + self.direction[0] * self.speed,
-                    self.pos[1] + self.direction[1] * self.speed)
+        self.pos = (self.pos[0] + self.direction[0] * self.speed*dt,
+                    self.pos[1] + self.direction[1] * self.speed*dt)
         
         # Check if bullet is out of bounds ( 800x600 place holder)
         if (self.pos[0] < 0 or self.pos[0] > 800 or
@@ -91,18 +93,34 @@ def main():
 
     screen = pygame.display.set_mode((800, 600))
     screen.fill((0, 0, 0))
+    
     clock = pygame.time.Clock()
     running = True
-    turret = Turret(pos=(400, 300), fire_rate=2)
-    bullets = []
     freaks = [Freak(pos=(random.randint(0, 800), random.randint(0, 600))) for _ in range(5)]
  
     while running :
+        #set delta time
+        dt = clock.tick(60) / 1000 
         #event loop
         for event in pygame.event.get():
             #use quit to close program
             if event.type == pygame.QUIT:
                 running = False
+        #update freaks
+        for freak in freaks:
+            freak.update(dt)
+
+        
+        #delete dead freaks
+        freaks = [freak for freak in freaks if freak.alive]
+
+        #draw freaks
+        for freak in freaks:
+            freak.draw(screen)
+
+        pygame.display.flip()
+        
+        
 
 if __name__ == "__main__":
     main()
