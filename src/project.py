6b1b99c07b9321d1056a0537f0d826fb7b696e9f
Vaughn Swanson <vaughnswanson@gmail.com>
2025-11-15 18:02:41 -0600

    created enemy health and speed generator

--- src/project.py
+++ src/project.py
@@ -1,12 +1,27 @@
 import pygame
 import random
 
+def EnemyHealthSpeedGenerator():
+    #give enemy random skill points from 1 to 5
+    EnemyTotalPoints = random.randint(1, 5)
+    #set base speed and health
+    health = 1
+    speed = 1
+    #distribute points randomly between health and speed
+    for points in EnemyTotalPoints:
+        if random.choice([True, False]):
+            health += 1
+        else:
+            speed += 1
+
+    return health, speed
+
 class Enemy():
 
-    def __init__(self, pos(0,0), health, speed):
+    def __init__ (self, pos(0,0), health, speed):
         self.pos = pos
-        self.health = health #add random health from 1-5
-        self.speed = speed # assign random speed from 1-3
+        self.health = health #add random health 
+        self.speed = speed # assign random speed 
         self.alive = True
 
     def update(self):
